4a658d055c32549052cd9a58e7188534f41a3299
Computer Science <computerscience@nicolass-air-3.home>
2018-01-09 10:56:49 +0000

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
-
-var data = {
-  id:10
-};
-
-var token = jwt.sign(data, '123abc');
-console.log(token)
+const bcrypt = require('bcryptjs');
 
 
-var decoded = jwt.verify(token,'123abc');
-console.log('Decoded', decoded);
+var password = '123abc!';
+
+// bcrypt.genSalt(10, (err, salt)=>{
+//   bcrypt.hash(password,salt, (err,hash)=>{
+//     console.log(hash);
+//   })
+// });
+
+var hashedPassword = '$2a$10$T7xEbWwvakgQN2RWABTPzeiXvqQxQE1uDMQ8S.zI18Dll3oAsl4EK'
+
+bcrypt.compare(password, hashedPassword, (err, res) =>{
+  console.log(res);
+})
+// var data = {
+//   id:10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token)
+//
+//
+// var decoded = jwt.verify(token,'123abc');
+// console.log('Decoded', decoded);
 
 
 // var message = " I am user number 3";
